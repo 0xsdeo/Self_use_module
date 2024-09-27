82dc4768d6e8eea4b7b3aaa13a6280aa1e1b8718
SpadeA-0 <i0xsdeo@foxmail.com>
2024-09-27 21:09:09 +0800

    Create get_data.py

---
+++ get_data.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+# @Project ：Self_use_module
+# @File    ：get_data.py
+# Author   ：0xsdeo
+# Date     ：2024/8/9 上午9:38
+
+def generate_data(file_name):
+    """
+
+    :param file_name: 传入文件名
+    :return: 以数组形式返回数据
+    """
+    with open(file_name, 'r', encoding='utf-8') as _:
+        data = _.readlines()
+
+    index = 0
+    for i in data:
+        data[index] = i.replace("\n", "")
+        index += 1
+
+    count = data.count('')
+
+    while count:  # 清除空字符
+        index = data.index('')
+        del data[index]
+        count -= 1
+
+    return data
+
+
+print(generate_data("dict.txt"))
